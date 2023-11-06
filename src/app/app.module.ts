c7d1159f3cf7953d1fe2ed9040af4aefe59538a1
Rain Raquipiso <87350585+rainraqui@users.noreply.github.com>
2023-11-06 20:19:57 +1300

    style changes, dark mode toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +10,16 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalculatorComponent
-  ],
+  declarations: [AppComponent, CalculatorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSlideToggleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent {
-  display: string = "";
+  display: string = '';
+  isDarkMode: boolean = false; // Default to light mode
 
   // Define button rows as an array of arrays
   buttonRows: string[][] = [
     ['7', '8', '9', '+'],
     ['4', '5', '6', '-'],
     ['1', '2', '3', '*'],
-    ['0', 'C', '=', '/']
+    ['0', 'C', '=', '/'],
   ];
 
   onButtonClick(value: string) {
@@ -27,14 +28,33 @@ export class CalculatorComponent {
   }
 
   clearDisplay() {
-    this.display = "";
+    this.display = '';
   }
 
   calculateResult() {
     try {
       this.display = eval(this.display);
     } catch (e) {
-      this.display = "Error";
+      this.display = 'Error';
     }
+  }
+
+  isOperator(button: string): boolean {
+    // Define logic to identify operator buttons
+    return ['+', '-', '*', '/'].includes(button);
+  }
+
+  isEquals(button: string): boolean {
+    // Define logic to identify the equals button
+    return button === '=';
+  }
+
+  isClear(button: string): boolean {
+    // Define logic to identify the clear button
+    return button === 'C';
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
   }
 }
